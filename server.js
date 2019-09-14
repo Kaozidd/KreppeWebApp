b555023599aa7326db574c0ad6c42f666e988911
kaozidd <arthur_hemingway@outlook.com>
2019-09-14 16:51:26 -0500

    Setting and configuring views

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 // Import libraries
 const express = require('express')
-const fs = require('fs')
 const app = express()
+const ejs = require('ejs')
 
 // Import Routers
 const pageRouter = require('./src/routers/pageRouter.js')
 const apiRouter = require('./src/routers/apiRouter.js')
+
+// Set view engine
+app.engine('ejs', ejs.renderFile)
+app.set('view engine', 'ejs')
+app.set('views', `${__dirname}/src/views`)
 
 // Set express folder
 app.use(express.static(`${__dirname}/public`))
@@ -16,7 +21,7 @@ app.use('/api', apiRouter)
 
 // Set 404 view
 app.use((req, res) => {
-	res.send('<h1>404 - Page Not Found!')
+	res.send('<h1>404 - Page Not Found!</h1>')
 })
 
 // Set PORT
--- src/routers/pageRouter.js
+++ src/routers/pageRouter.js
@@ -1,15 +1,12 @@
-const fs = require('fs')
 const Router = require('express').Router
 const pageRouter = Router()
 
 pageRouter.get('/', (req, res) => {
-	fs.readFile(`${__dirname}/src/views/home.html`, 'utf-8', (error, content) => {
-		res.send(content)
-	})
+		res.render('home.ejs')
 })
 
 pageRouter.get('/about', (req, res) => {
-  res.send('<h1>About page</h1>')
+		res.render('about.ejs')
 })
 
 module.exports = pageRouter
